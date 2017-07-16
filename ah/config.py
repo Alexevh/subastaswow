ff88d428b28e882aade02d6e4d4e188f56814411
Alexevh <Alexevh@users.noreply.github.com>
2017-07-16 18:36:04 -0300

    Create config.py

---
+++ ah/config.py
@@ -0,0 +1,10 @@
+config ={
+
+    'API-KEY':'yourkeyfromblizzard',
+    'SMTP_SERVER':'yourstmptserver.com',
+    'STMP_USER': 'alertas@yourserver.com',
+    'SMTP_PASSWORD': 'yourpasswordforemail',
+    'STMP_ORIGEN':'alertas@yourserver.com',
+    'SMTP_DESTINO':'youremaildestini@yourserver.com'
+
+}
