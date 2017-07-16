cdbde4ca7ad5c556a388b90711da26c006bc4076
Alex <alex2005.uy@gmail.com>
2017-07-16 18:43:04 -0300

    Comit prueba GIT

---
+++ config.example.py
@@ -0,0 +1,13 @@
+
+#Este archivo debe ser renombrado a config.py para funcionar
+
+config ={
+
+    'API-KEY':'yourkeyfromblizzard',
+    'SMTP_SERVER':'yourstmptserver.com',
+    'STMP_USER': 'alertas@yourserver.com',
+    'SMTP_PASSWORD': 'yourpasswordforemail',
+    'STMP_ORIGEN':'alertas@yourserver.com',
+    'SMTP_DESTINO':'youremaildestini@yourserver.com'
+
+}
